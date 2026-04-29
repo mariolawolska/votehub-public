ea4b05bb1a14080452643256a2db0e03e477e491
Mariola Wolska <47420914+mariolawolska@users.noreply.github.com>
2026-04-29 21:21:02 +0100

    Delete frontend/src directory

--- frontend/src/LogoutButton.jsx
+++
@@ -1,7 +0,0 @@
-import { useAuth } from "../context/AuthContext";
-
-export default function LogoutButton() {
-  const { logout } = useAuth();
-
-  return <button onClick={logout}>Logout</button>;
-}
